4440a7a0236764dca7925ffbb48eba2744fe75f4
Richard Garai <garric@okki.hu>
2021-04-05 18:41:42 +0200

    refactor

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"math/rand"
-	"os"
 	"time"
 )
 
@@ -26,20 +24,6 @@ func main() {
 	return
 }
 
-func printRolls(rolls []int) {
-	for _, r := range rolls {
-		fmt.Printf("%d\n", r)
-	}
-}
-
-func printSum(rolls []int) {
-	sum := 0
-	for _, r := range rolls {
-		sum += r
-	}
-	fmt.Printf("%d\n", sum)
-}
-
 func generateRolls(args []string) ([]int, error) {
 	rolls := make([]int, 0)
 	for _, s := range args {
@@ -50,29 +34,4 @@ func generateRolls(args []string) ([]int, error) {
 		}
 	}
 	return rolls, nil
-}
-
-func exitErr(err error) {
-	fmt.Fprintln(os.Stderr, err)
-	os.Exit(1)
-}
-
-func initFlags() {
-	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, "Usage:")
-		fmt.Fprintln(os.Stderr, "\troll <roll-def(s)>")
-		fmt.Fprintln(os.Stderr, "\t\troll-def: [count]d<sides>")
-		fmt.Fprintln(os.Stderr, "\t\tsides: [4,6,8,10,12,20,100]")
-		fmt.Fprintln(os.Stderr)
-		fmt.Fprintln(os.Stderr, "Examples:")
-		fmt.Fprintln(os.Stderr, "\td6\trolls one six-sided die")
-		fmt.Fprintln(os.Stderr, "\t1d10\trolls one ten-sided die")
-		fmt.Fprintln(os.Stderr, "\t3d4\trolls three four-sided dice")
-		fmt.Fprintln(os.Stderr, "\td4 d6\trolls a four-sided die and a six-sided die")
-		fmt.Fprintln(os.Stderr)
-		fmt.Fprintln(os.Stderr, "Flags:")
-		flag.PrintDefaults()
-	}
-	flag.BoolVar(&sumFlag, "s", false, "print a single line summing up all dice")
-	flag.Parse()
 }
